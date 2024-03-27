4bf9979b182886b0632f3bf84fefa7f77db3ae6f
LucasNBS <lucasnunesbarretosampaio@gmail.com>
2024-03-27 18:37:17 -0300

    feat: Filter by author

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from .models import Post
 from .forms import PostForm
@@ -7,10 +8,19 @@ from .forms import PostForm
 def list_posts(request):
   posts = Post.objects.all()
 
+  users = []
+
+  for post in posts:
+    if post.user not in users:
+      users.append(post.user)
+
   search_title = request.GET.get("search-title")
+  search_author = request.GET.get("search-author")
 
   if search_title is not None:
     posts = posts.filter(title__icontains=search_title)
+  if not search_author == "" and not search_author is None:
+    posts = posts.filter(user__username=search_author)
 
   paginator = Paginator(posts, 2)
 
@@ -21,12 +31,16 @@ def list_posts(request):
 
   if search_title is None:
     search_title = ""
+  if search_author is None:
+    search_author = ""
 
   page_obj = paginator.get_page(page)
 
   context = {
     'page_obj': page_obj,
     'search_title': search_title,
+    'users': users,
+    'search_author': search_author,
   }
 
   return render(request, 'posts/index.html', context)
